8f5f2e95272d057795181c1fb91514caadf53f9e
twenytoo <alushan1997@gmail.com>
2021-10-21 15:20:29 +0300

    added delete action

--- myPlaces_SB/View Controllers/MainViewController.swift
+++ myPlaces_SB/View Controllers/MainViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 import RealmSwift
 
 class MainViewController: UITableViewController {
-
+    
     var places: Results<Place>!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         places = realm.objects(Place.self)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
@@ -36,19 +36,31 @@ class MainViewController: UITableViewController {
         
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
-
+        
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath)
+    -> UISwipeActionsConfiguration? {
+        
+        let place = places[indexPath.row]
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) { (_, _, _) in
+            
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        deleteAction.image = UIImage(systemName: "trash")
+        deleteAction.backgroundColor = .systemRed
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
     }
-    */
+    
+    
+    //MARK: - Navigation
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
